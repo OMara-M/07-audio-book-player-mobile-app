c9bfba716e3cd6449bdd4e2ea9b39a7fdab7c2de
Matthew O'Mara <tuh39725@temple.edu>
2022-03-23 15:11:40 -0400

    add base functionality to booklist class

--- app/src/main/java/edu/temple/audiobookplayer/BookList.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookList.kt
@@ -1,4 +1,20 @@
 package edu.temple.audiobookplayer
 
-class BookList {
+class BookList (private val _books:ArrayList<Book>){
+
+    fun add(book:Book): Unit {
+        _books.add(book)
+    }
+
+    fun remove(book:Book): Unit{
+        _books.remove(book)
+    }
+
+    fun get(bookID: Int): Book {
+        return _books[bookID]
+    }
+
+    fun size(): Int {
+        return _books.size
+    }
 }
