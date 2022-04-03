c3571cb803c7addb2b2b150d0bbdefbc9abac1ac
Matthew O'Mara <tuh39725@temple.edu>
2022-04-03 15:10:24 -0400

    add viewholder class to list adapter

--- app/src/main/java/edu/temple/audiobookplayer/BookListAdapter.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookListAdapter.kt
@@ -1,4 +1,16 @@
 package edu.temple.audiobookplayer
 
-class BookListAdapter {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BookListAdapter (_books: BookList, _onClick: (Book) -> Unit) : RecyclerView.Adapter<BookListAdapter.BookViewHolder>(){
+
+    private val books = _books
+    private val onClick = _onClick
+
+    class BookViewHolder (layout : View): RecyclerView.ViewHolder (layout) {
+        val titleTextView : TextView = layout.findViewById(R.id.titleTextView)
+        val authorTextView : TextView = layout.findViewById(R.id.authorTextView)
+    }
 }
