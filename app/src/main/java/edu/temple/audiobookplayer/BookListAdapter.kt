140cdd99b2ac060cd0ec5f069264e78a47d1432c
Matthew O'Mara <tuh39725@temple.edu>
2022-04-03 15:13:37 -0400

    add on create view holder

--- app/src/main/java/edu/temple/audiobookplayer/BookListAdapter.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookListAdapter.kt
@@ -1,6 +1,8 @@
 package edu.temple.audiobookplayer
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,5 +14,17 @@ class BookListAdapter (_books: BookList, _onClick: (Book) -> Unit) : RecyclerVie
     class BookViewHolder (layout : View): RecyclerView.ViewHolder (layout) {
         val titleTextView : TextView = layout.findViewById(R.id.titleTextView)
         val authorTextView : TextView = layout.findViewById(R.id.authorTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        return BookViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return books.size()
     }
 }
