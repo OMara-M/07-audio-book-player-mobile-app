e6ed7b73bccc69c66bbcf001b17473add30a63a3
Matthew O'Mara <tuh39725@temple.edu>
2022-04-03 17:22:34 -0400

    add main not running

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -2,18 +2,16 @@ package edu.temple.audiobookplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity(), BookListFragment.BookSelect {
 
-    private val isSingleContainer : Boolean by lazy {
+    private val isDetails: Boolean by lazy {
         findViewById<View>(R.id.detailsFrag) == null
     }
 
-    private val BookViewModel : BookViewModel by lazy {
+    private val BookViewModel: BookViewModel by lazy {
         ViewModelProvider(this).get(BookViewModel::class.java)
     }
 
@@ -28,25 +26,42 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookSelect {
         }
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().add(R.id.listFrag, BookListFragment.newInstance(books)).commit()
-        }
+            supportFragmentManager.beginTransaction()
+                .add(R.id.listFrag, BookListFragment.newInstance(books)).commit()
+        } else
 
+            if (isDetails && BookViewModel.getSelectedBook().value != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.listFrag, BookDetailsFragment()).setReorderingAllowed(true)
+                    .addToBackStack(null).commit()
+            }
+        if (!isDetails && supportFragmentManager.findFragmentById(R.id.detailsFrag) !is BookDetailsFragment) {
+            supportFragmentManager.beginTransaction().add(R.id.detailsFrag, BookDetailsFragment())
+                .commit()
+        }
     }
 
-    private fun createBookList(): BookList{
+    override fun bookSelect() {
+        if (isDetails) {
+            supportFragmentManager.beginTransaction().replace(R.id.listFrag, BookDetailsFragment())
+                .setReorderingAllowed(true).addToBackStack(null).commit()
+        }
+    }
+
+    private fun createBookList(): BookList {
         val books = BookList()
         val titles = resources.getStringArray(R.array.titles)
         val authors = resources.getStringArray(R.array.authors)
-        books.add(Book(titles[0],authors[0]))
-        books.add(Book(titles[1],authors[1]))
-        books.add(Book(titles[2],authors[2]))
-        books.add(Book(titles[3],authors[3]))
-        books.add(Book(titles[4],authors[4]))
-        books.add(Book(titles[5],authors[5]))
-        books.add(Book(titles[6],authors[6]))
-        books.add(Book(titles[7],authors[7]))
-        books.add(Book(titles[8],authors[8]))
-        books.add(Book(titles[9],authors[9]))
+        books.add(Book(titles[0], authors[0]))
+        books.add(Book(titles[1], authors[1]))
+        books.add(Book(titles[2], authors[2]))
+        books.add(Book(titles[3], authors[3]))
+        books.add(Book(titles[4], authors[4]))
+        books.add(Book(titles[5], authors[5]))
+        books.add(Book(titles[6], authors[6]))
+        books.add(Book(titles[7], authors[7]))
+        books.add(Book(titles[8], authors[8]))
+        books.add(Book(titles[9], authors[9]))
         return books
     }
 }
