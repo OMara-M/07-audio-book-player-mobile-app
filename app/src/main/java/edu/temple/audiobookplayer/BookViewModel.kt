b944b710a8ad5d066585a229dc2696c8a175de17
Matthew O'Mara <tuh39725@temple.edu>
2022-04-03 15:02:29 -0400

    add functions to view model

--- app/src/main/java/edu/temple/audiobookplayer/BookViewModel.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookViewModel.kt
@@ -1,4 +1,19 @@
 package edu.temple.audiobookplayer
 
-class BookViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookViewModel : ViewModel() {
+    private val book: MutableLiveData<Book> by lazy {
+        MutableLiveData()
+    }
+
+    fun getSelectedBook(): LiveData<Book> {
+        return book
+    }
+
+    fun setSelectedBook(Book: Book?) {
+        this.book.value = Book
+    }
 }
