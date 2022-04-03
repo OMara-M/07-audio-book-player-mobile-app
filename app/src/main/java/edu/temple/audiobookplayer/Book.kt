7450a6c7e812b5f73d91e9bd131541190a917d51
Matthew O'Mara <tuh39725@temple.edu>
2022-04-03 14:03:54 -0400

    add parcelable memebers to book class

--- app/src/main/java/edu/temple/audiobookplayer/Book.kt
+++ app/src/main/java/edu/temple/audiobookplayer/Book.kt
@@ -1,4 +1,31 @@
 package edu.temple.audiobookplayer
 
-class Book (val _author:String, val _title:String) {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Book(val _author: String, val _title: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(_author)
+        parcel.writeString(_title)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Book> {
+        override fun createFromParcel(parcel: Parcel): Book {
+            return Book(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Book?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
